57ed8febec2b6e311409a21fed66ae301dace50c
Rajrup Das <rajrupdasid@outlook.com>
2021-06-20 00:31:42 +0530

    Database changed to Postgres for reliability

--- Login.cs
+++ Login.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
 
 namespace Employee_Management
 {
@@ -35,32 +34,7 @@ namespace Employee_Management
         private void button3_Click(object sender, EventArgs e)
         {
             try
-            {
-                string myConnection = "datasource=localhost;port=3306;username=root;password=";
-                MySqlConnection myConn = new MySqlConnection(myConnection);
-                MySqlCommand SelectCommand = new MySqlCommand("select * from emp.clientdetails where username='" + this.textBox1.Text + "' and password='"+this.textBox2.Text+"' ", myConn);
-                MySqlDataReader myReader;
-                myConn.Open();
-                myReader = SelectCommand.ExecuteReader();
-                int count = 0;
-                while (myReader.Read())
-                {
-                    count++;
-                }
-                if (count == 1)
-                {
-                    MessageBox.Show("Successfully login");
-                }
-                else if (count > 1)
-                {
-                    MessageBox.Show("Access denied");
-                }
-                else
-                {
-                    MessageBox.Show("Data Set not Exist");
-
-                }
-                myConn.Close();
+            { 
             }
             catch (Exception ex)
             {
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Employee_Management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
+            Application.Run(new Login());
         }
     }
 }
